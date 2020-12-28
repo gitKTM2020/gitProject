77270e5e11f8c1f2ff9ee698bb59afddc1f61140
Ameer <Ameer@DESKTOP-3V8DTOO>
2020-12-28 17:59:02 +0500

    Final Run

--- src/gitProject/main.java
+++ src/gitProject/main.java
@@ -4,6 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Ameer objameer = new Ameer();
+		Module1 objmod = new Module1();
+		Salman_1 objsolo= new Salman_1();
+		objsolo.getName();
 
 	}
 
