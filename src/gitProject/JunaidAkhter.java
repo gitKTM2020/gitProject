84fdf1f83ef438bb50cd8920c48d7f2b01ef105d
Muhammad Junaid <Muhammad Junaid@DESKTOP-9P6R7E0>
2020-12-28 15:10:25 -0800

    Add function is added

---
+++ src/gitProject/JunaidAkhter.java
@@ -0,0 +1,20 @@
+package gitProject;
+
+public class JunaidAkhter {
+
+	public static Integer a,b,c;
+	
+	public static void JunaidAkhter() {
+		a = 10;
+		b = 20;
+		c = 0;
+	}
+	public static void Add() {
+		c = a + b;
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
